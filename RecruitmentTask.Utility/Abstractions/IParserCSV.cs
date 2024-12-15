e99f95496e59b82e845524da505e272c17e959a0
Mateusz Kojtych <enonwow@gmail.com>
2024-12-15 15:03:18 +0100

    Cleanup solution

--- RecruitmentTask.Utility/Abstractions/IParserCSV.cs
+++
@@ -1,9 +0,0 @@
-﻿using RecruitmentTask.Core;
-
-namespace RecruitmentTask.Utility.Abstractions
-{
-    public interface IParserCSV
-    {
-        Task<List<Employee>> ReadEmployeesAsync(Stream stream);
-    }
-}
